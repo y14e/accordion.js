1f1743b37c5dcbad3100f247ad8220ed08d3c64f
Yusuke Kamiyamane <y@y14e.com>
2025-03-02 10:43:17 +0900

    20250302

--- accordion.ts
+++ accordion.ts
@@ -78,9 +78,9 @@ class Accordion {
       const opened = document.querySelector(`[aria-expanded="true"][data-accordion-name="${name}"]`) as HTMLElement;
       if (isOpen && opened && opened !== trigger) this.close(opened, isMatch);
     }
-    trigger.setAttribute('aria-expanded', String(isOpen));
     const item = trigger.closest(this.settings.selector.item) as HTMLElement_animation;
     const height = `${item.offsetHeight}px`;
+    trigger.setAttribute('aria-expanded', String(isOpen));
     item.style.setProperty('overflow', 'clip');
     item.style.setProperty('will-change', [...new Set(window.getComputedStyle(item).getPropertyValue('will-change').split(',')).add('height').values()].filter(value => value !== 'auto').join(','));
     if (item._animation) item._animation.cancel();
